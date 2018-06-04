61404b97fdd6d3e20ea09bd8ccd0d764bd936c7f
Apaar Madan <apaar_madan@hotmail.com>
2018-06-04 02:39:28 -0400

    Updated readme

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 if not defined DevEnvDir (
-    call "%VS_CMD_LINE_BUILD_PATH%" x86
+    call "%VS_CMD_LINE_BUILD_PATH%" x64
 )
 
 SET opts=-Zi /W3 /EHsc /Fewat.exe /D_CRT_SECURE_NO_WARNINGS
